960e0fa087fb470cc40fb967236fbda1b5fb69b3
CANVILLE Sebastien (SNCF / SURETE FERROVIAIRE / SUGE BORDEAUX) <8407285d@commun.ad.sncf.fr>
2023-02-02 10:44:14 +0100

    préaration du dépot avec dossier et app module complet + main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,25 @@ import { UsersModule } from './users/users.module';
 import { ServicesModule } from './services/services.module';
 import { ReservationsModule } from './reservations/reservations.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [UsersModule, ServicesModule, ReservationsModule, AuthModule],
+  imports: [UsersModule, ServicesModule, ReservationsModule, AuthModule,
+    ConfigModule.forRoot(), 
+    TypeOrmModule.forRoot({  
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: false,
+      logging: false
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {constructor(private dataSource: DataSource) {}}
