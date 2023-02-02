32a2ea3057c9dfed492a1d93499b1a425908d272
CANVILLE Sebastien (SNCF / SURETE FERROVIAIRE / SUGE BORDEAUX) <8407285d@commun.ad.sncf.fr>
2023-02-02 10:07:42 +0100

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ServicesModule } from './services/services.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [UsersModule, ServicesModule, ReservationsModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
