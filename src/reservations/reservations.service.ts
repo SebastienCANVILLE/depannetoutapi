b8cf05eb4e435fe99b7037902eb52adccc176bcd
CANVILLE Sebastien (SNCF / SURETE FERROVIAIRE / SUGE BORDEAUX) <8407285d@commun.ad.sncf.fr>
2023-02-02 14:51:53 +0100

    mise à jour

--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -3,7 +3,7 @@ import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
 
 @Injectable()
-export class Reservation {
+export class ReservationsService {
   create(createReservationDto: CreateReservationDto) {
     return 'This action adds a new reservation';
   }
